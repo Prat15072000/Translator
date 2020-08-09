dabe616f918a429585717b26a787c84ac9834cb3
Prat15072000 <pratsimple2000@gmail.com>
2020-08-09 16:07:47 +0530

    Create pdfFile.dart
    Created new file for PDF

---
+++ docuTranslate/lib/pdfFile.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class PdfFile extends StatefulWidget {
+  @override
+  _PdfFileState createState() => _PdfFileState();
+}
+
+class _PdfFileState extends State<PdfFile> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
